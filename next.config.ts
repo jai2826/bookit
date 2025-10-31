57edecfda3629e76b52de4e63083822592a6c0f9
Jai Lakhmani <jailakhmani12345@gmail.com>
2025-10-31 21:01:18 +0530

    feat: refactor next.config.ts to integrate PrismaPlugin and streamline webpack configuration

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,25 @@
-// next.config.js
-
-// Import the plugin
 const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
+  experimental: {
+    // Disables the default Turbopack build
+    // This allows your custom 'webpack' config to run without error.
+    forceSwcTransforms: true, // Forces usage of the underlying SWC compiler which respects the Webpack hook
+  },
+  webpack: (config: any, { isServer }: { isServer: boolean }) => {
+    // We only want to run this customization for the server-side build
+    // (which becomes the Vercel Serverless Function)
+    if (isServer) {
+      // Add the plugin to the existing list of Webpack plugins
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+
+    // Always return the final configuration object
+    return config;
+  },
+
   images: {
     // List the domains you are pulling images from
     remotePatterns: [
@@ -23,20 +38,6 @@ const nextConfig = {
       },
     ];
   },
-  // Any other Next.js config options here (like output: 'standalone')
-
-  // The 'webpack' key is how you access the underlying Webpack configuration
-  webpack: (config: any, { isServer }: { isServer: boolean }) => {
-    // We only want to run this customization for the server-side build
-    // (which becomes the Vercel Serverless Function)
-    if (isServer) {
-      // Add the plugin to the existing list of Webpack plugins
-      config.plugins = [...config.plugins, new PrismaPlugin()];
-    }
-
-    // Always return the final configuration object
-    return config;
-  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
